5eb534eb99962242aa2e577cb5b4fd91e8777764
muchachooos <odnorogg@bk.ru>
2022-08-24 17:43:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,37 +4,51 @@ import (
 	"fmt"
 )
 
+var i int
+
 type Element struct {
 	value int
 	next  *Element
 }
 
 func main() {
-	element4 := Element{
-		4,
-		nil,
-	}
-
 	element3 := Element{
 		3,
-		&element4,
+		nil,
 	}
 
 	element2 := Element{
 		2,
-		&element3,
+		&element4,
 	}
 
 	element1 := Element{
 		1,
+		&element3,
+	}
+
+	element0 := Element{
+		0,
 		&element2,
 	}
-	print(&element1)
-	add(&element1, 5)
-	add(&element1, 6)
-	print(&element1)
-	delete(&element1)
-	print(&element1)
+	print(&element0)
+	set(&element0, 3, 5)
+}
+
+// Заменяет value элемента под номером num на val
+func set(root *Element, num int, val int) {
+	if root == nil {
+		fmt.Println("")
+		return
+	}
+
+	if num == i {
+		root.value = val
+	} else {
+		i++
+		set(root.next, num, val)
+	}
+	i = 0
 }
 
 func print(root *Element) {
@@ -62,11 +76,6 @@ func add(root *Element, val int) {
 		return
 	}
 	add(root.next, val)
-}
-
-// Заменяет value элемента под номером num на val
-func set(root *Element, num int, val int) {
-
 }
 
 // Заменяет значение элемента под номером number на val
